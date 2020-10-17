932fa23e8d63392d9986c5f6030a1f2e105a0852
stevenharja <stevenharja@gmail.com>
2020-10-17 19:40:26 +1100

    Updates

---
+++ precache-manifest.149261eda81d92f23eae794766a8b16c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3250cda6f76df8b65f07909aaf2c43ab",
+    "url": "/travelloging-react/index.html"
+  },
+  {
+    "revision": "fdcf2554fca3fecd68fc",
+    "url": "/travelloging-react/static/css/main.ad84474d.chunk.css"
+  },
+  {
+    "revision": "880b1f743ee6672d9b5e",
+    "url": "/travelloging-react/static/js/2.e4aef61c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/travelloging-react/static/js/2.e4aef61c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdcf2554fca3fecd68fc",
+    "url": "/travelloging-react/static/js/main.023e82cf.chunk.js"
+  },
+  {
+    "revision": "c6fc297459c466d7ad28",
+    "url": "/travelloging-react/static/js/runtime-main.9b95e8d8.js"
+  },
+  {
+    "revision": "334556a2bc6b1e9ff4cd313909087799",
+    "url": "/travelloging-react/static/media/author_pic.334556a2.jpg"
+  },
+  {
+    "revision": "1fdfda239ead702bcfd8b88bb4a46ea9",
+    "url": "/travelloging-react/static/media/bg-img.1fdfda23.jpg"
+  },
+  {
+    "revision": "8c35288a5ed0afada0edcfcdfc83fdf6",
+    "url": "/travelloging-react/static/media/blog_post.8c35288a.jpg"
+  },
+  {
+    "revision": "23b802d7ed3a186df39aab9da2b45f11",
+    "url": "/travelloging-react/static/media/blog_post2.23b802d7.jpg"
+  },
+  {
+    "revision": "343cfecf826f789a7d8a040168d71d01",
+    "url": "/travelloging-react/static/media/blog_post3.343cfecf.jpg"
+  },
+  {
+    "revision": "623785a6d39c867358f3600bce641ba5",
+    "url": "/travelloging-react/static/media/camera-retro.623785a6.svg"
+  },
+  {
+    "revision": "3670d21c993c27cf41b600fc72b7838b",
+    "url": "/travelloging-react/static/media/person-bg.3670d21c.png"
+  }
+]);
